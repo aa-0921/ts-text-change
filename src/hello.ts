783b62e10f1a62c5cd81b9bbb503553d54e038f1
akuto aoyama <aaaa.akuto@gmail.com>
2020-05-18 08:38:31 +0900

    srcフォルダ追加

---
+++ src/hello.ts
@@ -0,0 +1,12 @@
+// const message: string = 'Hello! TypeScript!';
+// console.log(message);
+
+function getValue() {
+    // var $formObject = event.target.value;
+    // document.getElementById("output").innerHTML = $formObject;
+
+
+    var $formObject = document.getElementById( "input-form" );
+    document.getElementById("output").innerHTML = $formObject.inputForm.value;
+
+}
