a11b471dd7477d5892cda5f1fc9a655b09902445
akuto aoyama <aaaa.akuto@gmail.com>
2020-05-18 09:22:14 +0900

    .prettierrc追加

--- src/hello.ts
+++ src/hello.ts
@@ -2,11 +2,10 @@
 // console.log(message);
 
 function getValue() {
-    // var $formObject = event.target.value;
-    // document.getElementById("output").innerHTML = $formObject;
+  // var $formObject = event.target.value;
+  // document.getElementById("output").innerHTML = $formObject;
 
 
-    var $formObject = document.getElementById("input-form");
-    document.getElementById("output").innerHTML = $formObject.inputForm.value;
-
+  const $formObject = document.getElementById('input-form');
+  document.getElementById('output').innerHTML = $formObject.inputForm.value;
 }
