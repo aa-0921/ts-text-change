9b393bc87a157f86aab2ea090dae707fb46a42e1
akuto aoyama <aaaa.akuto@gmail.com>
2020-05-18 13:15:35 +0900

    script.ts修正

--- src/script.ts
+++ src/script.ts
@@ -1,6 +1,3 @@
-// const message: string = 'Hello! TypeScript!';
-// console.log(message);
-
 function getValue(): void {
   const $formObject = document.getElementById('input-form') as HTMLFormElement;
   document.getElementById('output')!.innerHTML = $formObject.inputForm.value;
